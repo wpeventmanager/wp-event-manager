5fa7e21eda1f85053b4482696e73fc9a8ba541e5
hitesh <hitesh@localhost-hitesh>
2018-08-30 15:13:05 +0530

    Datacleaner called in unitall file

--- core/wp-event-manager-data-cleaner.php
+++ core/wp-event-manager-data-cleaner.php
@@ -62,7 +62,61 @@ class WP_Event_Manager_Data_Cleaner {
 	 * @var $site_options
 	 */
 	private static $site_options = array(
-		'event_manager_helper',
+			'wp_event_manager_version',
+			
+			'event_manager_per_page',
+			
+			'event_manager_hide_cancelled_events',
+			
+			'event_manager_hide_expired_content',
+			
+			'event_manager_enable_categories',
+			
+			'event_manager_enable_event_types',
+			
+			'event_manager_enable_event_ticket_prices',
+			
+			'event_manager_enable_default_category_multiselect',
+			
+			'event_manager_enable_default_event_type_multiselect',
+			
+			'event_manager_category_filter_type',
+			
+			'event_manager_event_type_filter_type',
+			
+			'event_manager_user_requires_account',
+			
+			'event_manager_enable_registration',
+			
+			'event_manager_registration_role',
+			
+			'event_manager_submission_requires_approval',
+			
+			'event_manager_user_can_edit_pending_submissions',
+			
+			'event_manager_submission_expire_options',
+			
+			'event_manager_submission_duration',
+			
+			'event_manager_allowed_registration_method',
+			
+			'event_manager_submit_event_form_page_id',
+			
+			'event_manager_event_dashboard_page_id',
+			
+			'event_manager_events_page_id',
+			
+			'event_manager_installed_terms',
+			
+			'event_manager_submit_page_slug',
+			
+			'event_manager_event_dashboard_page_slug',
+			
+			'event_manager_rating_showcase_admin_notices_dismiss',
+			
+			'event_manager_google_maps_api_key',
+			
+			'event_manager_backend_form_fields'
 	);
 
 	/**
