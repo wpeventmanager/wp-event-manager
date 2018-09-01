2ade2d6da197e063105a93eeac282628e2fb335a
Ashok Dudhat <>
2018-09-01 17:32:34 +0200

    changed comment

--- external/external.php
+++ external/external.php
@@ -3,7 +3,7 @@
  * Load external compatibility tweaks.
  */
 
-// Load language files
+// Load external files
 require_once( 'wpml.php' );
 require_once( 'polylang.php' );
 require_once( 'all-in-one-seo-pack.php' );
