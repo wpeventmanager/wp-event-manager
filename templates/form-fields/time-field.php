d40b6fa0fd1f60a701f2f242d2d3c71052c9b558
hitesh <hitesh@localhost-hitesh>
2019-01-08 15:31:26 +0530

    event submission js updated with date format and timepicker. Time picker field type updated according to the timepicker js

--- templates/form-fields/time-field.php
+++ templates/form-fields/time-field.php
@@ -1,7 +1,11 @@
 <?php 
 /**
-* Get event managet template because we want to override this template from the template folder
-* 
-**/
-get_event_manager_template( 'event-time.php',array('field' => $field ,'key' => $key) ); 
+ * 
+ * @since 3.0
+ * 
+ */
 ?>
+<div class="controls" style="position: relative">
+   <input type="text" class="input-text" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>" id="<?php echo esc_attr( $key ); ?>" attribute="<?php echo esc_attr( isset( $field['attribute'] ) ? $field['attribute'] : '' ); ?>" placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>" value="<?php echo isset( $field['value'] ) ? esc_attr( $field['value'] ) : ''; ?>" maxlength="<?php echo ! empty( $field['maxlength'] ) ? $field['maxlength'] : ''; ?>" <?php if ( ! empty( $field['required'] ) ) echo 'required'; ?> data-picker="timepicker" />
+   <?php if ( ! empty( $field['description'] ) ) : ?><small class="description"><?php echo $field['description']; ?></small><?php endif; ?>
+</div>
