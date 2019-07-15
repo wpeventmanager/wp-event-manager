b1bfe6b5816b70b89f3c260e69ead9b0e9b41c4d
hitesh <hitesh@hitesh-H110M-S2>
2019-07-15 14:44:26 +0530

    view more button added in organizer template

--- templates/form-fields/repeated-field.php
+++ templates/form-fields/repeated-field.php
@@ -103,42 +103,6 @@
 
         </div>
 
-        <?php /*
-        <ul class="nav nav-tabs">
-            <li class="pull-right"><a href="#remove" class="remove-row" id="repeated-row-<?php echo esc_attr( $key.'_%%repeated-row-index%%' ); ?>" ><?php _e( 'Remove', 'wp-event-manager' ); ?></a></li>
-            <li class="active"><a data-toggle="tab" href="#sell-ticket-details_%%repeated-row-index%%"><?php _e('Ticket Details','wp-event-manager');?></a></li>
-            <li><a data-toggle="tab" href="#<?php echo $key ;?>_%%repeated-row-index%%"><?php _e('Settings','wp-event-manager');?></a></li>
-            
-          </ul>
-            <div class="tab-content">
-                <div id="sell-ticket-details_%%repeated-row-index%%" class="tab-pane fade in active">
-                <?php  foreach ( $field['fields'] as $subkey => $subfield ) : 
-                                if ($subkey == 'ticket_description') : ?>           
-                </div><!------------end ticket details tab------>
-                <div id="<?php echo $key; ?>_%%repeated-row-index%%" class="tab-pane fade">
-                <?php endif;?>
-                
-                    <fieldset class="fieldset-<?php esc_attr_e( $subkey ); ?>">
-    					<?php if(!empty($subfield['label'])) : ?>
-    					<div class="col-md-4"> <label for="<?php esc_attr_e( $subkey ); ?>"><?php echo $subfield['label'] . ( $subfield['required'] ? '' : ' <small>' . __( '(optional)', 'wp-event-manager' ) . '</small>' ); ?></label></div>
-    					<?php endif; ?>
-    					<div class="col-md-8">
-    					<div class="field">
-    					 <div>
-    						<?php							
-    							$subfield['name']  = $key . '_' . $subkey . '_%%repeated-row-index%%';
-    							$subfield['id']  = $key . '_' . $subkey . '_%%repeated-row-index%%';	
-    							get_event_manager_template( 'form-fields/' . $subfield['type'] . '-field.php', array( 'key' => $subkey, 'field' => $subfield ) );
-    						?>
-    						</div>
-    					</div>
-    					</div>
-    				</fieldset>   
-    		<?php endforeach; ?>
-                </div><!------------end settings tab------>     
-		</div>
-
-        */?>
 
 	<?php
 	echo esc_attr( ob_get_clean() );
