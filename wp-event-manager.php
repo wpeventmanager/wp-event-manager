98e30bb65b978d9185dec1518e68936acf2fc685
hitesh <gam.hitesh.makvana@gmail.com>
2019-11-24 11:11:21 +0530

    version updated

--- wp-event-manager.php
+++ wp-event-manager.php
@@ -80,7 +80,7 @@ class WP_Event_Manager {
 	public function __construct() 
 	{
 		// Define constants
-		define( 'EVENT_MANAGER_VERSION', '3.1.9' );
+		define( 'EVENT_MANAGER_VERSION', '3.1.10' );
 		define( 'EVENT_MANAGER_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 		define( 'EVENT_MANAGER_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
