a705e842809e54d3aba8743b5a4b7dac4597dc9d
wpem-kishan <wpem.kishan.kothari@gmail.com>
2020-06-29 10:07:54 +0530

    add organizer details on single event page using elementor widget

--- external/elementor.php
+++ external/elementor.php
@@ -84,6 +84,7 @@ class Plugin {
 		require_once( __DIR__ . '/elementor-widgets/elementor-event-dashboard.php' );
 		require_once( __DIR__ . '/elementor-widgets/elementor-past-event-listing.php' );
 		require_once( __DIR__ . '/elementor-widgets/elementor-single-event-field.php' );
+		require_once( __DIR__ . '/elementor-widgets/elementor-single-event-organizer.php' );
 	}
 
 	/**
@@ -105,6 +106,7 @@ class Plugin {
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Elementor_Event_Dashboard() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Elementor_Past_Event_Listing() );				
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Elementor_Event_Field() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Elementor_Single_Event_Organizer() );
 	}
 
 	/**
