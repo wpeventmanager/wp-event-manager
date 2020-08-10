cb2fa4c3b014b325ec4531dd485d95ee32d9d082
wpem-kishan <wpem.kishan.kothari@gmail.com>
2020-08-10 15:47:20 +0530

    Fixed #298 Need to change text on organizer form submit

--- templates/organizer-submitted.php
+++ templates/organizer-submitted.php
@@ -4,11 +4,11 @@ global $wp_post_types;
 switch ( $organizer->post_status ) :
 
 	case 'publish' :
-		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s listed successfully. To view your listing <a href="%s">click here</a>.', 'wp-event-manager' ).'</p>', $wp_post_types['event_organizer']->labels->singular_name, get_permalink( $organizer->ID ) );
+		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s listed successfully. To view your organizer list <a href="%s">click here</a>.', 'wp-event-manager' ).'</p>', $wp_post_types['event_organizer']->labels->singular_name, get_permalink( $organizer->ID ) );
 	break;
 	
 	case 'pending' :
-		printf( '<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s submitted successfully. Your listing will be visible once approved.', 'wp-event-manager' ).'</p>', $wp_post_types['event_organizer']->labels->singular_name, get_permalink( $organizer->ID ) );
+		printf( '<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s submitted successfully. Your organizer will be visible once approved.', 'wp-event-manager' ).'</p>', $wp_post_types['event_organizer']->labels->singular_name, get_permalink( $organizer->ID ) );
 	break;
 
 	default :
--- templates/venue-submitted.php
+++ templates/venue-submitted.php
@@ -4,11 +4,11 @@ global $wp_post_types;
 switch ( $venue->post_status ) :
 
 	case 'publish' :
-		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s listed successfully. To view your listing <a href="%s">click here</a>.', 'wp-event-manager' ).'</p>', $wp_post_types['event_venue']->labels->singular_name, get_permalink( $venue->ID ) );
+		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s listed successfully. To view your venue list <a href="%s">click here</a>.', 'wp-event-manager' ).'</p>', $wp_post_types['event_venue']->labels->singular_name, get_permalink( $venue->ID ) );
 	break;
 	
 	case 'pending' :
-		printf( '<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s submitted successfully. Your listing will be visible once approved.', 'wp-event-manager' ).'</p>', $wp_post_types['event_organizer']->labels->singular_name, get_permalink( $venue->ID ) );
+		printf( '<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__( '%s submitted successfully. Your venue will be visible once approved.', 'wp-event-manager' ).'</p>', $wp_post_types['event_organizer']->labels->singular_name, get_permalink( $venue->ID ) );
 	break;
 
 	default :
