e1fa4abb9bbfcb70f8f71cdd2fa319841e3f7ca5
wpem-kishan <wpem.kishan.kothari@gmail.com>
2020-08-25 15:58:18 +0530

    Fixed #423 Database upgrade issue

--- wp-event-manager.php
+++ wp-event-manager.php
@@ -81,7 +81,6 @@ class WP_Event_Manager {
 	{
 		// Define constants
 		define( 'EVENT_MANAGER_VERSION', '3.1.15' );
-		define( 'EVENT_MANAGER_DB_VERSION', '3.1.15' );
 		define( 'EVENT_MANAGER_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 		define( 'EVENT_MANAGER_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
