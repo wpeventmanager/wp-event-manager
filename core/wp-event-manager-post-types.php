e4a4536a3ed1d7131cc948053506fce65721455c
wpem-kishan <wpem.kishan.kothari@gmail.com>
2020-10-02 10:20:55 +0530

    Fixed #555 Need to replace word Edit Post to Edit Organizer in admin side.

--- core/wp-event-manager-post-types.php
+++ core/wp-event-manager-post-types.php
@@ -435,6 +435,8 @@ class WP_Event_Manager_Post_Types {
 						'singular_name' 		=> $singular,
 
 						'add_new_item' 			=> sprintf( __( 'Add %s', 'wp-event-manager' ), $singular ),
+
+						'edit_item' 			=> sprintf( __( 'Edit %s', 'wp-event-manager' ), $singular ),
 						
 						'featured_image'        => __( 'Organizer Logo', 'wp-event-manager' ),
 						
