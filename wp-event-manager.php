83d02186869ea7c148d44b06cf8a34f21f1c3247
hiteshmakvana <makvanahitesh33@gmail.com>
2021-02-19 16:53:58 +0530

    wrong text domain updated in mo files

--- wp-event-manager.php
+++ wp-event-manager.php
@@ -181,7 +181,7 @@ class WP_Event_Manager {
 
         	$locale = apply_filters('plugin_locale', get_locale(), $domain);
 
-		load_textdomain( $domain, WP_LANG_DIR . "/wp-event-manager-stripe-split-payment/".$domain."-" .$locale. ".mo" );
+		load_textdomain( $domain, WP_LANG_DIR . "/wp-event-manager/".$domain."-" .$locale. ".mo" );
 
 		load_plugin_textdomain($domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
