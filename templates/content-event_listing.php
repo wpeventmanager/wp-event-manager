34cdf74cb5d4bfe6b5f2d65ef5733056680b25ad
hiteshmakvana <makvanahitesh33@gmail.com>
2022-02-14 16:01:17 +0530

    Improved box layout image background #938

--- templates/content-event_listing.php
+++ templates/content-event_listing.php
@@ -17,7 +17,7 @@ $thumbnail  = get_event_thumbnail($post,'full');
         <div <?php event_listing_class(''); ?>>
             <a href="<?php display_event_permalink(); ?>" class="wpem-event-action-url event-style-color <?php echo $event_type; ?>">
                 <div class="wpem-event-banner">
-                    <div class="wpem-event-banner-img" style="background-image: url('<?php echo $thumbnail ?>')">
+                    <div class="wpem-event-banner-img" style="background-image: url(<?php echo $thumbnail ?>)">
 
                         <!-- Hide in list View // Show in Box View -->
                         <?php do_action('event_already_registered_title'); ?>     
