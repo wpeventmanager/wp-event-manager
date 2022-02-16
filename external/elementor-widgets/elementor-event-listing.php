2fa9075c8dee986fdd627e0686073cdacf3735ed
Aamir <blog.aamir@gmail.com>
2022-02-16 17:21:43 +0530

    Elementor cancelled events are not showing up on listing page #960

--- external/elementor-widgets/elementor-event-listing.php
+++ external/elementor-widgets/elementor-event-listing.php
@@ -325,7 +325,7 @@ class Elementor_Event_Listing extends Widget_Base {
         $featured = !empty($settings['featured']) ? ' featured="'.$settings['featured'].'"' : '';
         $cancelled = !empty($settings['cancelled']) ? ' cancelled="'.$settings['cancelled'].'"' : '';
             
-        $shortcode = '[events show_pagination="'.$settings['show_pagination'].'" per_page="'.$settings['per_page'].'" order="'.$settings['order'].'" orderby="'.$settings['orderby'].'" featured="'.$settings['featured'].'" cancelled="'.$settings['cancelled'].'" show_filters="'.$settings['show_filters'].'" show_categories="'.$settings['show_categories'].'" show_event_types="'.$settings['show_event_types'].'" '.$location.' '.$keywords.' '.$categories.' '.$event_types.' '.$selected_datetime.' ]';
+        $shortcode = '[events show_pagination="'.$settings['show_pagination'].'" per_page="'.$settings['per_page'].'" order="'.$settings['order'].'" orderby="'.$settings['orderby'].'" '.$featured.' '.$cancelled.' show_filters="'.$settings['show_filters'].'" show_categories="'.$settings['show_categories'].'" show_event_types="'.$settings['show_event_types'].'" '.$location.' '.$keywords.' '.$categories.' '.$event_types.' '.$selected_datetime.' ]';
 
         echo do_shortcode($shortcode);
 	}
