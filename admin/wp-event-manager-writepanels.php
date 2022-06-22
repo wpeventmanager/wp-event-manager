87fa2df8e541bcbdcc9a92df6263e6a459d955e5
Aamir <blog.aamir@gmail.com>
2022-06-22 16:53:56 +0530

    Field editor- Time zone type is display blank #1095

--- admin/wp-event-manager-writepanels.php
+++ admin/wp-event-manager-writepanels.php
@@ -707,7 +707,7 @@ class WP_Event_Manager_Writepanels
 			<select name="<?php echo esc_attr(isset($field['name']) ? $field['name'] : $key); ?>" id="<?php echo isset($field['id']) ? esc_attr($field['id']) : esc_attr($key); ?>" class="input-select <?php echo esc_attr(isset($field['class']) ? $field['class'] : $key); ?>">
 				<?php
 				$value = isset($field['value']) ? $field['value'] : $field['default'];
-		echo esc_attr(WP_Event_Manager_Date_Time::wp_event_manager_timezone_choice($value));
+				echo WP_Event_Manager_Date_Time::wp_event_manager_timezone_choice(esc_attr($value));
 				?>
 			</select>
 		</p>
