863569c531e669bc53a42f8e55e74b0c4ba868ff
Yukta Gelani <kinjukathiriya6810@gmail.com>
2022-09-01 14:13:19 +0530

    https://github.com/wpeventmanager/wp-event-manager/issues/1201

--- templates/organizer/content-single-event_organizer.php
+++ templates/organizer/content-single-event_organizer.php
@@ -1,5 +1,4 @@
 <?php $organizer = get_post($organizer_id);
-//$organizer_fields =    get_metadata('post', $organizer_id);
 if (get_option('event_manager_form_fields')) {
     $organizer_fields = get_option('event_manager_form_fields', true)['organizer'];
 }
@@ -11,15 +10,14 @@ $organizer_email = get_post_meta($organizer_id, '_organizer_email', true)
 
         <?php do_action('single_event_listing_organizer_start'); ?>
         <div class="wpem-row">
-
+        
             <div class="wpem-col-md-3">
                 <div class="wpem-organizer-logo-wrapper">
                     <div class="wpem-organizer-logo">
                         <a><?php display_organizer_logo('', '', $organizer); ?></a>
                     </div>
-                    <?php /** <div class="wpem-organizer-logo-title wpem-heading-text"><a><span><?php echo esc_attr($organizer_name); ?></span></a></div> */ ?>
-
-                </div>
+                    <?php /** <div class="wpem-organizer-logo-title wpem-heading-text"><a><span><?php echo $organizer_name; ?></span></a></div> */ ?>
+                    </div>
             </div>
 
             <div class="wpem-col-md-9 wpem-col-sm-12">
@@ -29,56 +27,56 @@ $organizer_email = get_post_meta($organizer_id, '_organizer_email', true)
                         <span><?php echo esc_html($organizer->post_title); ?></span>
                     </div>
 
-                    <div class="wpem-organizer-description"><?php $description = the_content();
-                    printf(__('%s', 'wp-event-manager'), wp_kses_post( $description )); ?></div>
+                    <div class="wpem-organizer-description"><?php $description = the_content(); ?></div>
 
+                  
                     <div class="wpem-organizer-social-links">
                         <div class="wpem-organizer-social-lists">
                             <?php do_action('single_event_listing_organizer_social_start', $organizer_id); ?>
                             <?php
-                            //get disable organizer fields
-                            $organizer_fields = get_hidden_form_fields( 'event_manager_submit_organizer_form_fields', 'organizer');
+                                                       //get disable organizer fields
+                                                       $organizer_fields = get_hidden_form_fields( 'event_manager_submit_organizer_form_fields', 'organizer');
 
-                            $organizer_website  = !in_array('organizer_website', $organizer_fields)?get_organizer_website($organizer):'';
-                            $organizer_facebook = !in_array('organizer_facebook', $organizer_fields)?get_organizer_facebook($organizer):'';
-                            $organizer_instagram = !in_array('organizer_instagram', $organizer_fields)?get_organizer_instagram($organizer):'';
-                            $organizer_twitter  = !in_array('organizer_twitter', $organizer_fields)?get_organizer_twitter($organizer):'';
-                            $organizer_youtube  = !in_array('organizer_youtube', $organizer_fields)?get_organizer_youtube($organizer):'';
-                            ?>
-                            <?php
-                            if (!empty($organizer_website)) {
-                            ?>
+                                                       $organizer_website  = !in_array('organizer_website', $organizer_fields)?get_organizer_website($organizer):'';
+                                                       $organizer_facebook = !in_array('organizer_facebook', $organizer_fields)?get_organizer_facebook($organizer):'';
+                                                       $organizer_instagram = !in_array('organizer_instagram', $organizer_fields)?get_organizer_instagram($organizer):'';
+                                                       $organizer_twitter  = !in_array('organizer_twitter', $organizer_fields)?get_organizer_twitter($organizer):'';
+                                                       $organizer_youtube  = !in_array('organizer_youtube', $organizer_fields)?get_organizer_youtube($organizer):'';
+                                                       ?>
+                                                       <?php
+                                                       if (!empty($organizer_website)) {
+                                                       ?>
                                 <div class="wpem-social-icon wpem-weblink">
                                     <a href="<?php echo esc_url($organizer_website); ?>" target="_blank" title="<?php _e('Get Connect on Website', 'wp-event-manager'); ?>"><?php _e('Website', 'wp-event-manager'); ?></a>
                                 </div>
-                            <?php
+                                <?php
                             }
 
                             if (!empty($organizer_facebook)) {
-                            ?>
+                                ?> 
                                 <div class="wpem-social-icon wpem-facebook">
                                     <a href="<?php echo esc_url($organizer_facebook); ?>" target="_blank" title="<?php _e('Get Connect on Facebook', 'wp-event-manager'); ?>"><?php _e('Facebook', 'wp-event-manager'); ?></a>
                                 </div>
-                            <?php
+                                <?php
                             }
 
                             if (!empty($organizer_instagram)) {
-                            ?>
+                                ?> 
                                 <div class="wpem-social-icon wpem-instagram">
                                     <a href="<?php echo esc_url($organizer_instagram); ?>" target="_blank" title="<?php _e('Get Connect on Instagram', 'wp-event-manager'); ?>"><?php _e('Instagram', 'wp-event-manager'); ?></a>
                                 </div>
-                            <?php
+                                <?php
                             }
 
                             if (!empty($organizer_twitter)) {
-                            ?>
+                                ?>
                                 <div class="wpem-social-icon wpem-twitter">
                                     <a href="<?php echo esc_url($organizer_twitter); ?>" target="_blank" title="<?php _e('Get Connect on Twitter', 'wp-event-manager'); ?>"><?php _e('Twitter', 'wp-event-manager'); ?></a>
                                 </div>
-                            <?php
+                                <?php
                             }
                             if (!empty($organizer_youtube)) {
-                            ?>
+                                ?>
                                 <div class="wpem-social-icon wpem-youtube">
                                     <a href="<?php echo esc_url($organizer_youtube); ?>" target="_blank" title="<?php _e('Get Connect on Youtube', 'wp-event-manager'); ?>"><?php _e('Youtube', 'wp-event-manager'); ?></a>
                                 </div>
@@ -101,15 +99,28 @@ $organizer_email = get_post_meta($organizer_id, '_organizer_email', true)
                     <?php endforeach;
                     } ?>
                     <?php do_action('submit_organizer_form_organizer_fields_end'); ?>
-                    <div class="wpem-organizer-contact-actions">
+                    
+                </div>
+            </div>
+
+            <div class="wpem-col-md-9 wpem-col-sm-12">
+                <div class="wpem-organizer-infomation-wrapper">
+
+                    <div class="wpem-organizer-name wpem-heading-text">
+                        <span><?php echo $organizer->post_title; ?></span>
+                    </div>
+
+                    <div class="wpem-organizer-description"><?php printf(__('%s', 'wp-event-manager'), get_organizer_description($organizer)); ?></div>
+                    
+                    <div class="wpem-organizer-contact-actions">                    
                         <?php do_action('single_event_listing_organizer_action_start', $organizer_id); ?>
 
                         <?php do_action('single_event_listing_organizer_action_end', $organizer_id); ?>
                     </div>
-
+            
                 </div>
             </div>
-
+            
         </div>
 
         <?php do_action('single_event_listing_organizer_end'); ?>
@@ -117,8 +128,8 @@ $organizer_email = get_post_meta($organizer_id, '_organizer_email', true)
 </div>
 
 <?php
-get_event_manager_template(
-    'organizer-event_listing.php',
+get_event_manager_template( 
+    'organizer-event_listing.php', 
     array(
         'organizer_id'    => $organizer_id,
         'per_page'        => $per_page,
@@ -127,7 +138,7 @@ get_event_manager_template(
         'currentEvents'   => $currentEvents,
         'pastEvents'      => $pastEvents,
         'current_page'    => $current_page,
-    ),
-    'wp-event-manager/organizer',
+    ), 
+    'wp-event-manager/organizer', 
     EVENT_MANAGER_PLUGIN_DIR . '/templates/organizer/'
 );
