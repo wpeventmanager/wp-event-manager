9a6567a520eb40a656ac05f9be8448370bfa311e
Hiren <sangani.reeta@gmail.com>
2022-09-05 16:43:26 +0530

    Addons menu- Few plugins are missing in the list #1197

--- admin/wp-event-manager-addons.php
+++ admin/wp-event-manager-addons.php
@@ -21,52 +21,54 @@ class WP_Event_Manager_Addons
 	 * Handles output of the reports page in admin.
 	 */
 
-	public function output()
-	{
-
-		if (false === ($addons = get_transient('wp_event_manager_addons_html'))) {
-
-			$raw_addons = wp_remote_get(
-					'http://www.wp-eventmanager.com/plugins',
-					array(
-							'timeout'     => 10,
-							'redirection' => 5,
-							'sslverify'   => false
-					)
-					);
-
-			if (!is_wp_error($raw_addons)) {
-
-				$raw_addons = wp_remote_retrieve_body($raw_addons);
-
-				// Get Products
-				$dom = new DOMDocument();
-				libxml_use_internal_errors(true);
-				$dom->loadHTML($raw_addons);
-
-				$xpath = new DOMXPath($dom);
-				$tags  = $xpath->query('//ul[@class="products columns-4"]');
-				foreach ($tags as $tag) {
-					$addons = $tag->ownerDocument->saveXML($tag);
-					break;
-				}
-
-				$addons = wp_kses_post($addons);
-
-				if ($addons) {
-					set_transient('wp_event_manager_addons_html1', $addons, 60 * 60 * 24 * 7); // Cached for a week
-				}
-			}
-		}
-
-		?>
+	public function output() {?>
 		<div class="wrap wp_event_manager wp_event_manager_addons_wrap">
-				<h2><?php _e('WP Event Manager Add-ons', 'wp-event-manager'); ?></h2>
-
-			<?php echo $addons; ?>
+		<h2><?php _e('WP Event Manager Add-ons', 'wp-event-manager'); ?></h2>
+		<?php $url = array(
+			'http://www.wp-eventmanager.com/plugins',
+			'http://www.wp-eventmanager.com/plugins/page/2',
+			'http://www.wp-eventmanager.com/plugins/page/3'
+		);
+		
+		if (false === ($addons = get_transient('wp_event_manager_addons_html'))) {
+			for( $i = 0; $i <= 2; $i++ ){
+				
+				$raw_addons = wp_remote_get(
+					$url[$i],
+						array(
+								'timeout'     => 10,
+								'redirection' => 5,
+								'sslverify'   => false
+						)
+						);
+					
+				if (!is_wp_error($raw_addons)) {
+					$raw_addons = wp_remote_retrieve_body($raw_addons);
+	
+					// Get Products
+					$dom = new DOMDocument();
+					libxml_use_internal_errors(true);
+					$dom->loadHTML($raw_addons);
+	
+					$xpath = new DOMXPath($dom);
+					$tags  = $xpath->query('//ul[@class="products columns-4"]');
+					foreach ($tags as $tag) {
+						$addons = $tag->ownerDocument->saveXML($tag);
+						break;
+					}
+	
+					$addons = wp_kses_post($addons);
+					if ($addons) {
+						set_transient('wp_event_manager_addons_html1', $addons, 60 * 60 * 24 * 7); // Cached for a week
+					}
+				}
+				echo $addons; 
+			}
+		} ?>
 		</div>
-		<?php
-	}
+	<?php
+	} 
 }
+
 endif;
 return new WP_Event_Manager_Addons();
