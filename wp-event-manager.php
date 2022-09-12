51ecba2c14470901e18e31c952de7e63ab75e876
Krinay Dhanani <dhananikrinay@gmail.com>
2022-09-12 09:15:04 +0200

    #1217
    Readme and version updated.

--- wp-event-manager.php
+++ wp-event-manager.php
@@ -14,7 +14,7 @@ Text Domain: wp-event-manager
 
 Domain Path: /languages
 
-Version: 3.1.31
+Version: 3.1.32
 
 Since: 1.0.0
 
@@ -80,7 +80,7 @@ class WP_Event_Manager {
 	public function __construct() 
 	{
 		// Define constants
-		define('EVENT_MANAGER_VERSION', '3.1.31');
+		define('EVENT_MANAGER_VERSION', '3.1.32');
 		define( 'EVENT_MANAGER_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 		define( 'EVENT_MANAGER_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
