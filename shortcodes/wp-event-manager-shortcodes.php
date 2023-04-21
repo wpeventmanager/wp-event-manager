3825c2b5edd2dbe6fc2a62027e39c320552f4db3
Hiren <sangani.reeta@gmail.com>
2023-04-21 13:10:53 +0530

    Organizer dashboard message should be proper #1327

--- shortcodes/wp-event-manager-shortcodes.php
+++ shortcodes/wp-event-manager-shortcodes.php
@@ -401,10 +401,11 @@ class WP_Event_Manager_Shortcodes
 
 		if (!is_user_logged_in()) {
 
-			ob_start();
-
-			get_event_manager_template('event-dashboard-login.php');
-
+			ob_start(); ?>
+			<div id="event-manager-event-dashboard">
+				<p class="account-sign-in wpem-alert wpem-alert-info"><?php _e( 'You need to be signed in to manage your organizer listings.', 'wp-event-manager' ); ?> <a href="<?php echo apply_filters( 'event_manager_event_dashboard_login_url', esc_url(get_option('event_manager_login_page_url'),esc_url(wp_login_url())) ); ?>"><?php _e( 'Sign in', 'wp-event-manager' ); ?></a></p>
+			</div>
+			<?php 
 			return ob_get_clean();
 		}
 
@@ -557,10 +558,11 @@ class WP_Event_Manager_Shortcodes
 
 		if (!is_user_logged_in()) {
 
-			ob_start();
-
-			get_event_manager_template('event-dashboard-login.php');
-
+			ob_start();	?>
+			<div id="event-manager-event-dashboard">
+				<p class="account-sign-in wpem-alert wpem-alert-info"><?php _e( 'You need to be signed in to manage your venue listings.', 'wp-event-manager' ); ?> <a href="<?php echo apply_filters( 'event_manager_event_dashboard_login_url', esc_url(get_option('event_manager_login_page_url'),esc_url(wp_login_url())) ); ?>"><?php _e( 'Sign in', 'wp-event-manager' ); ?></a></p>
+			</div>
+			<?php 
 			return ob_get_clean();
 		}
 
