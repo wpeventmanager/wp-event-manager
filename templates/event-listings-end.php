143a3aa74baebd8ce3f081f492e8fccfc7f4f1bd
Hiren <sangani.reeta@gmail.com>
2023-05-24 13:58:02 +0530

    Load more button not visible in the List/box layout when calendar addon installed #1392

--- templates/event-listings-end.php
+++ templates/event-listings-end.php
@@ -1,20 +1,22 @@
 
 </div>
-<?php if($show_filters){
-	if (!$show_pagination && $show_more) {
-		echo '<div id="load_more_events_loader">';
-			echo wp_kses_post('<a class="load_more_events" id="load_more_events" href="#" style="display:none;"><strong>' . __('Load more events', 'wp-event-manager') . '</strong></a>');
-		echo '</div>';
-	}
-}else{
-	if ($events->found_posts > $per_page && $show_more) :
-		if ($show_pagination) :
-			echo wp_kses_post(get_event_listing_pagination($events->max_num_pages));?>
-		<?php else :?>
-			<div id="load_more_events_loader">
-				<a class="load_more_events" id="load_more_events" href="#" ><strong><?php _e('Load more listings', 'wp-event-manager'); ?></strong></a>
-			</div>
-		<?php endif;
-	endif;
-} ?>
+<?php if(isset($show_filters)) {
+	if($show_filters){
+		if (!$show_pagination && $show_more) {
+			echo '<div id="load_more_events_loader">';
+				echo wp_kses_post('<a class="load_more_events" id="load_more_events" href="#" style="display:none;"><strong>' . __('Load more events', 'wp-event-manager') . '</strong></a>');
+			echo '</div>';
+		}
+	}else{
+		if ($events->found_posts > $per_page && $show_more) :
+			if ($show_pagination) :
+				echo wp_kses_post(get_event_listing_pagination($events->max_num_pages));?>
+			<?php else :?>
+				<div id="load_more_events_loader">
+					<a class="load_more_events" id="load_more_events" href="#" ><strong><?php _e('Load more listings', 'wp-event-manager'); ?></strong></a>
+				</div>
+			<?php endif;
+		endif;
+	} 
+}?>
 </div>
