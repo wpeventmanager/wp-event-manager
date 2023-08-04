46facd7e1aac13c7bf0e02e0fe97c9c06f547657
Hiren <sangani.reeta@gmail.com>
2023-08-04 13:35:39 +0530

    Both layout type are display on the page with [upcoming_events] shortcode #1046

--- templates/event-listings-start.php
+++ templates/event-listings-start.php
@@ -1,27 +1,28 @@
-<?php wp_enqueue_script('wp-event-manager-content-event-listing'); 
-if ($layout_type == 'all') : ?>
-    <div class="wpem-main wpem-event-listings-header">
-        <div class="wpem-row">
-            <div class="wpem-col wpem-col-12 wpem-col-sm-6 wpem-col-md-6 wpem-col-lg-8">
-                <div class="wpem-event-listing-header-title">
-                    <?php if(isset($title)) : ?>
-                        <h2 class="wpem-heading-text"><?php printf(__('%s', 'wp-event-manager'), $title);?></h2>
-                    <?php endif; ?>
-                </div>
+<?php wp_enqueue_script('wp-event-manager-content-event-listing'); ?>
+<div class="wpem-main wpem-event-listings-header">
+    <div class="wpem-row">
+        <div class="wpem-col wpem-col-12 wpem-col-sm-6 wpem-col-md-6 wpem-col-lg-8">
+            <div class="wpem-event-listing-header-title">
+                <?php if(isset($title)) : ?>
+                    <h2 class="wpem-heading-text"><?php printf(__('%s', 'wp-event-manager'), $title);?></h2>
+                <?php endif; ?>
             </div>
-            <div class="wpem-col wpem-col-12 wpem-col-sm-6 wpem-col-md-6 wpem-col-lg-4">
-                <div class="wpem-event-layout-action-wrapper">
-                    <div class="wpem-event-layout-action">
+        </div>
+        <div class="wpem-col wpem-col-12 wpem-col-sm-6 wpem-col-md-6 wpem-col-lg-4">
+            <div class="wpem-event-layout-action-wrapper">
+                <div class="wpem-event-layout-action">
+                    <?php if ($layout_type == 'all') :  ?>
                         <?php do_action('start_event_listing_layout_icon'); ?>
                         <div class="wpem-event-layout-icon wpem-event-box-layout" title="<?php _e('Events Box View', 'wp-event-manager'); ?>" id="wpem-event-box-layout"><i class="wpem-icon-stop2"></i></div>
                         <div class="wpem-event-layout-icon wpem-event-list-layout wpem-active-layout" title="<?php _e('Events List View', 'wp-event-manager'); ?>" id="wpem-event-list-layout"><i class="wpem-icon-menu"></i></div>
                         <?php do_action('end_event_listing_layout_icon'); ?>
-                    </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
     </div>
-<?php endif; ?>
+</div>
+
 <!-- Event listing view -->
 <?php
 if ($layout_type == 'box')
