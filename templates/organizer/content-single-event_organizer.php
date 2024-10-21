21f908ac5034b8a5809272c99204212b0ec23735
Roshani <roshani.koradiya264@gmail.com>
2024-10-21 15:38:24 +0530

    #1517 Hide data from single organizer page

--- templates/organizer/content-single-event_organizer.php
+++ templates/organizer/content-single-event_organizer.php
@@ -7,7 +7,17 @@ if (get_option('event_manager_form_fields') && is_array(get_option('event_manage
     $GLOBALS['event_manager']->forms->get_form( 'submit-organizer', array() );
     $form_submit_organizer_instance = call_user_func( array( 'WP_Event_Manager_Form_Submit_Organizer', 'instance' ) );
     $organizer_custom_fields = $form_submit_organizer_instance->merge_with_custom_fields( 'backend' );
-} ?>
+} 
+// Check user is loggedin or not.
+$check_user_access 	 = wpem_checked_guest_user_access();
+
+// If user not logged in hide some fields from that
+if ( $check_user_access == false ) {
+    $field_to_hide = get_option( 'hide_organizer_fields' );
+} else {
+	$field_to_hide = array();
+}
+?>
 
 <div class="wpem-single-organizer-profile-wrapper" id="wpem_organizer_profile">
     <div class="wpem-organizer-profile">
@@ -17,7 +27,11 @@ if (get_option('event_manager_form_fields') && is_array(get_option('event_manage
             <div class="wpem-col-md-3">
                 <div class="wpem-organizer-logo-wrapper">
                     <div class="wpem-organizer-logo">
-                        <a><?php display_organizer_logo('', '', $organizer); ?></a>
+                        <a><?php
+                            if(!in_array('organizer_logo', $field_to_hide)) {
+                                display_organizer_logo('', '', $organizer); 
+                            }
+                            ?></a>
                     </div>
                 </div>
             </div>
@@ -25,11 +39,19 @@ if (get_option('event_manager_form_fields') && is_array(get_option('event_manage
             <div class="wpem-col-md-9 wpem-col-sm-12">
                 <div class="wpem-organizer-infomation-wrapper">
                     <div class="wpem-organizer-name wpem-heading-text">
-                        <span><?php echo esc_attr($organizer->post_title); ?></span>
+                        <span><?php 
+                        if(!in_array('organizer_name', $field_to_hide)) {
+                            echo esc_attr($organizer->post_title); 
+                        }
+                        ?></span>
                     </div>
                     <div class="wpem-organizer-description">
-                        <?php $content = apply_filters('wpem_the_content',$organizer->post_content);
-                        echo wp_kses_post($content);?>
+                        <?php 
+                        if(!in_array('organizer_description', $field_to_hide)) {
+                            $content = apply_filters('wpem_the_content',$organizer->post_content);
+                            echo wp_kses_post($content);
+                        }
+                        ?>
                      </div>
                     <div class="wpem-organizer-social-links">
                         <div class="wpem-organizer-social-lists">
@@ -43,30 +65,30 @@ if (get_option('event_manager_form_fields') && is_array(get_option('event_manage
                             $organizer_twitter  = !in_array('organizer_twitter', $organizer_fields)?get_organizer_twitter($organizer):'';
                             $organizer_youtube  = !in_array('organizer_youtube', $organizer_fields)?get_organizer_youtube($organizer):'';
                           
-                            if (!empty($organizer_website)) { ?>
+                            if (!empty($organizer_website) && !in_array('organizer_website', $field_to_hide) ) { ?>
                                 <div class="wpem-social-icon wpem-weblink">
                                     <a href="<?php echo esc_url($organizer_website); ?>" target="_blank" title="<?php esc_attr_e('Get Connect on Website', 'wp-event-manager'); ?>"><?php esc_html_e('Website', 'wp-event-manager'); ?></a>
                                 </div>
                             <?php }
 
-                            if (!empty($organizer_facebook)) { ?>
+                            if (!empty($organizer_facebook) && !in_array('organizer_facebook', $field_to_hide) ) { ?>
                                 <div class="wpem-social-icon wpem-facebook">
                                     <a href="<?php echo esc_url($organizer_facebook); ?>" target="_blank" title="<?php esc_attr_e('Get Connect on Facebook', 'wp-event-manager'); ?>"><?php esc_html_e('Facebook', 'wp-event-manager'); ?></a>
                                 </div>
                             <?php  }
 
-                            if (!empty($organizer_instagram)) { ?>
+                            if (!empty($organizer_instagram) && !in_array('organizer_instagram', $field_to_hide) ) { ?>
                                 <div class="wpem-social-icon wpem-instagram">
                                     <a href="<?php echo esc_url($organizer_instagram); ?>" target="_blank" title="<?php esc_attr_e('Get Connect on Instagram', 'wp-event-manager'); ?>"><?php esc_html_e('Instagram', 'wp-event-manager'); ?></a>
                                 </div>
                             <?php }
 
-                            if (!empty($organizer_twitter)) { ?>
+                            if (!empty($organizer_twitter) && !in_array('organizer_twitter', $field_to_hide) ) { ?>
                                 <div class="wpem-social-icon wpem-twitter">
                                     <a href="<?php echo esc_url($organizer_twitter); ?>" target="_blank" title="<?php esc_attr_e('Get Connect on Twitter', 'wp-event-manager'); ?>"><?php esc_html_e('Twitter', 'wp-event-manager'); ?></a>
                                 </div>
                             <?php  }
-                            if (!empty($organizer_youtube)) { ?>
+                            if (!empty($organizer_youtube) && !in_array('organizer_youtube', $field_to_hide)) { ?>
                                 <div class="wpem-social-icon wpem-youtube">
                                     <a href="<?php echo esc_url($organizer_youtube); ?>" target="_blank" title="<?php esc_attr_e('Get Connect on Youtube', 'wp-event-manager'); ?>"><?php esc_html_e('Youtube', 'wp-event-manager'); ?></a>
                                 </div>
