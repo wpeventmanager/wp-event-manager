8c8f77eab69bc90c507c884530df1007e7003a0d
Roshani <roshani.koradiya264@gmail.com>
2024-11-06 17:00:56 +0530

    #1725 New organizer and venue is not created

--- templates/venue/venue-submitted.php
+++ templates/venue/venue-submitted.php
@@ -3,10 +3,10 @@ global $wp_post_types;
 //print appropriate message accroding to post status or post description
 switch ($venue->post_status) :
 	case 'publish' :
-		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.esc_attr('%s listed successfully. To view your venue list <a href="%s">click here</a>.', 'wp-event-manager').'</p>', esc_attr($wp_post_types['event_venue']->labels->singular_name), esc_url(get_permalink($venue->ID)));
+		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__('%s listed successfully. To view your venue list <a href="%s">click here</a>.', 'wp-event-manager').'</p>', esc_attr($wp_post_types['event_venue']->labels->singular_name), esc_url(get_permalink($venue->ID)));
 		break;
 	case 'pending' :
-		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.esc_attr('%s submitted successfully. Your venue will be visible once approved.', 'wp-event-manager').'</p>', esc_attr($wp_post_types['event_organizer']->labels->singular_name), esc_url(get_permalink($venue->ID)));
+		printf('<p class="post-submitted-success-green-message wpem-alert wpem-alert-success">'.__('%s submitted successfully. Your venue will be visible once approved.', 'wp-event-manager').'</p>', esc_attr($wp_post_types['event_organizer']->labels->singular_name), esc_url(get_permalink($venue->ID)));
 		break;
 	default :
 		do_action('event_manager_venue_submitted_content_' . str_replace('-', '_', sanitize_title($venue->post_status)), $venue);
