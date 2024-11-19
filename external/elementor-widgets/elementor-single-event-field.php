8d19dc065d4a95066cc5f90fbf19f6236d5a6248
Roshani <roshani.koradiya264@gmail.com>
2024-11-19 13:31:14 +0530

    #1747 Video URL field issue

--- external/elementor-widgets/elementor-single-event-field.php
+++ external/elementor-widgets/elementor-single-event-field.php
@@ -246,7 +246,7 @@ class Elementor_Event_Field extends Widget_Base {
                                 <div class="wpem-modal-header-close"><a href="javascript:void(0)" class="wpem-modal-close" id="wpem-modal-close">x</a></div>
                             </div>
                             <div class="wpem-modal-content">
-                                <?php echo wp_kses_post(wp_oembed_get(get_organizer_youtube(), array('autoplay' => 1, 'rel' => 0))); ?>
+                                <?php echo wp_oembed_get(get_organizer_youtube(), array('autoplay' => 1, 'rel' => 0)); ?>
                             </div>
                         </div>
                         <a href="#"><div class="wpem-modal-overlay"></div></a>
