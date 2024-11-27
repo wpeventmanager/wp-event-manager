666028282e7e78cd8d739ef6a5fd6caa5363b97e
Roshani <roshani.koradiya264@gmail.com>
2024-11-27 11:08:52 +0530

    #1751 Without login click on the event dashboard then text message is missing

--- templates/event-dashboard-login.php
+++ templates/event-dashboard-login.php
@@ -1,4 +1,5 @@
 <div id="event-manager-event-dashboard">
+	<?php esc_html_e('You need to be signed in to manage your listings.', 'wp-event-manager'); ?> 
 	<p class="account-sign-in wpem-alert wpem-alert-info">
 		<a href="<?php  echo !empty(get_option('event_manager_login_page_url')) ? esc_url(apply_filters('submit_event_form_login_url', get_option('event_manager_login_page_url'))) : 	esc_url(home_url() . '/wp-login.php'); ?>">
 			<?php esc_html_e('Sign in', 'wp-event-manager'); ?>
