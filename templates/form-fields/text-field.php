a8c0b52ff23bd84f377529d03b0673acffb5faa0
Nishant <91460148+officialnishantdhiman@users.noreply.github.com>
2025-01-21 11:55:42 +0530

    #1758
    #1758

--- templates/form-fields/text-field.php
+++ templates/form-fields/text-field.php
@@ -15,7 +15,7 @@
     class="input-text <?php echo esc_attr(isset($field['class']) ? $field['class'] : $key); ?>" 
     name="<?php echo esc_attr(isset($field['name']) ? $field['name'] : $key); ?>" 
     id="<?php echo isset($field['id']) ? esc_attr($field['id']) : esc_attr($key); ?>" 
-    placeholder="<?php echo empty($field['placeholder']) ? '' : esc_attr($field['placeholder'], 'wp-event-manager'); ?>" 
+    placeholder="<?php echo empty($field['placeholder']) ? '' : esc_html_e($field['placeholder'], 'wp-event-manager'); ?>" 
     attribute="<?php echo esc_attr(isset($field['attribute']) ? $field['attribute'] : ''); ?>" 
     value="<?php echo esc_attr(isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '')); ?>" 
     maxlength="<?php echo !empty($field['maxlength']) ? esc_attr($field['maxlength']) : ''; ?>" 
--- templates/form-fields/textarea-field.php
+++ templates/form-fields/textarea-field.php
@@ -1,4 +1,4 @@
-<textarea cols="20" rows="3" class="input-text" name="<?php echo esc_attr(isset($field['name']) ? $field['name'] : $key); ?>" id="<?php echo isset($field['id']) ? esc_attr($field['id']) :  esc_attr($key); ?>" attribute="<?php echo esc_attr(isset($field['attribute']) ? $field['attribute'] : ''); ?>" placeholder="<?php echo esc_attr($field['placeholder']) ?>" maxlength="<?php echo !empty($field['maxlength']) ? esc_attr($field['maxlength']) : ''; ?>" <?php if (!empty($field['required']))  echo esc_attr('required'); ?>>
+<textarea cols="20" rows="3" class="input-text" name="<?php echo esc_attr(isset($field['name']) ? $field['name'] : $key); ?>" id="<?php echo isset($field['id']) ? esc_attr($field['id']) :  esc_attr($key); ?>" attribute="<?php echo esc_attr(isset($field['attribute']) ? $field['attribute'] : ''); ?>" placeholder="<?php esc_html_e($field['placeholder']) ?>" maxlength="<?php echo !empty($field['maxlength']) ? esc_attr($field['maxlength']) : ''; ?>" <?php if (!empty($field['required']))  echo esc_attr('required'); ?>>
     <?php echo isset($field['value']) ? esc_textarea($field['value']) : ''; ?>
 </textarea>
 <?php if (!empty($field['description'])) : ?>
