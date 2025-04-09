1d64cb69ddf13480249bc80325aa2af202d08dfe
officialnishantdhiman <officialnishantdhiman@gmail.com>
2025-04-09 16:24:33 +0530

    #1535

--- forms/wp-event-manager-form-edit-event.php
+++ forms/wp-event-manager-form-edit-event.php
@@ -140,7 +140,7 @@ class WP_Event_Manager_Form_Edit_Event extends WP_Event_Manager_Form_Submit_Even
 					if (empty($custom_message)) {
 						$custom_message = __('Your changes have been saved.', 'wp-event-manager') . ' <a href="' . esc_url(get_permalink($this->event_id)) . '">' . __('View &rarr;', 'wp-event-manager') . '</a>';
 					} else {
-						$custom_message = str_replace('%view_link%', '<a href="' . esc_url(get_permalink($this->event_id)) . '">' . __('View &rarr;', 'wp-event-manager') . '</a>', $custom_message);
+						$custom_message = $custom_message . ' <a href="' . esc_url(get_permalink($this->event_id)) . '">' . __('View &rarr;', 'wp-event-manager') . '</a>';
 					}
 					echo wp_kses_post('<div class="event-manager-message wpem-alert wpem-alert-success">' . $custom_message . '</div>');
 					break;
