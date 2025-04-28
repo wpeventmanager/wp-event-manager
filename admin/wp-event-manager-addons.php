7746aff49c8dc9b749d80110c5a313765ae4925a
officialnishantdhiman <officialnishantdhiman@gmail.com>
2025-04-28 13:26:03 +0530

    #1506

--- admin/wp-event-manager-addons.php
+++ admin/wp-event-manager-addons.php
@@ -22,7 +22,7 @@ if(!class_exists('WP_Event_Manager_Addons')) :
 			<h2 style="display: none;"><?php esc_attr_e('WP Event Manager Add-ons', 'wp-event-manager'); ?></h2>
 				<div class="wpem-admin-extensions-page-header">
 					<img src="<?php echo esc_url(EVENT_MANAGER_PLUGIN_URL . '/assets/images/wpem-logo.svg'); ?>" alt="<?php esc_attr_e('WP Event Manager', 'wp-event-manager'); ?>" />
-					<button class="wpem-admin-theme-button wpem-admin-extensions-help-button"><i class="wpem-icon-info"></i> <?php esc_attr_e('Help', 'wp-event-manager'); ?></button>
+					<a href="https://wp-eventmanager.com/help-center/" class="wpem-admin-theme-button wpem-admin-extensions-help-button"><i class="wpem-icon-info"></i> <?php esc_attr_e('Help', 'wp-event-manager'); ?></a>
 				</div>
 				<div class="wpem-admin-tabs">
 					<ul class="wpem-admin-tabs-list">
