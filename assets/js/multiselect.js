ffdca71ebc51de5982e17390bf6df6deac9e6430
officialnishantdhiman <officialnishantdhiman@gmail.com>
2025-08-08 16:58:30 +0530

    #1901
    Multi select field required validation should be show as tooltip without refresh #1901

--- assets/js/multiselect.js
+++ assets/js/multiselect.js
@@ -10,37 +10,6 @@ var MultiSelect= function () {
         init: function() 
         {
 			jQuery(".event-manager-multiselect").chosen({search_contains:!0});
-            const $allSelects = jQuery('select.event-manager-multiselect[multiple]');
-
-            $allSelects.each(function() {
-                const $select = jQuery(this);
-                const $form = $select.closest('form');
-                $form.on('submit', function(e) {
-                    let selectedValues = $select.val();
-                    const $submitButton = jQuery(this).find('[type="submit"]');
-                    $select.closest('.field').find('.error-msg').remove();
-
-                    if (!selectedValues || selectedValues.length === 0) {
-                        e.preventDefault();
-                        $select.closest('.field').append('<div class="error-msg" style="color:red; font-size:13px; margin-top:5px;">Please fill this field</div>');
-                        $select.next('.chosen-container').css('border', '1px solid red');
-                        $submitButton.css('pointer-events', 'none');
-                    } else {
-                        $select.next('.chosen-container').css('border', '');
-                        $submitButton.css('pointer-events', 'auto');
-                    }
-                });
-                $select.on('change', function() {
-                    const selectedValues = jQuery(this).val();
-                    const $submitButton = $form.find('[type="submit"]');
-
-                    if (selectedValues && selectedValues.length > 0) {
-                        jQuery(this).closest('.field').find('.error-msg').remove();
-                        jQuery(this).next('.chosen-container').css('border', '');
-                        $submitButton.css('pointer-events', 'auto');
-                    }
-                });
-            });
 		}   
     } //enf of returnmultiselect
 }; //end of class
