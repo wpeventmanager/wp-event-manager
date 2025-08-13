f43256de6f3b5de016f3bb1e9235ca61782fa53a
officialnishantdhiman <officialnishantdhiman@gmail.com>
2025-08-13 14:15:40 +0530

    #1924
    Fontside : Event create shows warning when add from logout #1924

--- wp-event-manager-template.php
+++ wp-event-manager-template.php
@@ -668,15 +668,15 @@ function display_event_banner($size = 'full', $default = null, $post = null){
 		if($size !== 'full') {
 			$banner = event_manager_get_resized_image($banner, $size);
 		}
-		printf('<link rel="image_src" href="' . esc_attr($banner) . '"/>');
-		printf('<img itemprop="image" content="' . esc_attr($banner) . '" src="' . esc_attr($banner) . '" alt="' . esc_attr($alt_text) . '" />');
+		echo('<link rel="image_src" href="' . esc_attr($banner) . '"/>');
+		echo('<img itemprop="image" content="' . esc_attr($banner) . '" src="' . esc_attr($banner) . '" alt="' . esc_attr($alt_text) . '" />');
 	} else if($default) {
 
-		printf('<img itemprop="image" content="' . esc_attr($default) . '" src="' . esc_attr($default) . '" alt="' . esc_attr($alt_text) . '" />');
+		echo('<img itemprop="image" content="' . esc_attr($default) . '" src="' . esc_attr($default) . '" alt="' . esc_attr($alt_text) . '" />');
 	} else if(is_array($banner) && isset($banner[0])) {
-		printf('<img itemprop="image" content="' . esc_attr($banner[0]) . '" src="' . esc_attr($banner[0]) . '" alt="' . esc_attr($alt_text) . '" />');
+		echo('<img itemprop="image" content="' . esc_attr($banner[0]) . '" src="' . esc_attr($banner[0]) . '" alt="' . esc_attr($alt_text) . '" />');
 	} else {
-		printf('<img itemprop="image" content="' . esc_attr(apply_filters('event_manager_default_event_banner', EVENT_MANAGER_PLUGIN_URL . '/assets/images/wpem-placeholder.jpg')) . '" src="' . esc_attr(apply_filters('event_manager_default_event_banner', EVENT_MANAGER_PLUGIN_URL . '/assets/images/wpem-placeholder.jpg')) . '" alt="' . esc_attr($alt_text) . '" />');
+		echo('<img itemprop="image" content="' . esc_attr(apply_filters('event_manager_default_event_banner', EVENT_MANAGER_PLUGIN_URL . '/assets/images/wpem-placeholder.jpg')) . '" src="' . esc_attr(apply_filters('event_manager_default_event_banner', EVENT_MANAGER_PLUGIN_URL . '/assets/images/wpem-placeholder.jpg')) . '" alt="' . esc_attr($alt_text) . '" />');
 	}
 }
 
