bf15b9384f5c327fac66a5befdbdb11fa6d2a1f4
wpemhusengit <wpem.husen.multani@gmail.com>
2026-03-06 11:07:08 +0530

    show html tag in single event page #231 #2109
    When i create an event add description then we have publish and save then show the html tag with description on single event page

--- templates/content-single-event_listing.php
+++ templates/content-single-event_listing.php
@@ -111,7 +111,9 @@ $event = $post; ?>
                             <!-- Event description section start-->
                             <div class="wpem-single-event-body-content">
                                 <?php do_action('single_event_overview_start'); 
-                                echo esc_attr(apply_filters('wpem_the_content', $event->post_content)); 
+                               if(!empty($event->post_content)){
+                                    echo wp_kses_post(apply_filters('wpem_the_content', $event->post_content)); 
+                                }
                                 do_action('single_event_overview_end'); ?>
                             </div>
                             <!-- Event description section end-->
